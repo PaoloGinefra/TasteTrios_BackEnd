6468f4a3efa4945d44a2f6416e247645d711a8b9
Paolo Ginefra <ginefra.paolo@gmail.com>
2024-12-13 16:49:57 +0100

    fix: fix limit query

--- app.py
+++ app.py
@@ -89,20 +89,16 @@ def matchIngredients():
         with driver.session() as session:
             ingredients = request.json['ingredients']
             limitString = ""
-            if "limit" in request.json:
+            if ("limit" in request.json):
                 limit = request.json['limit']
                 limitString = f" LIMIT {limit}"
 
-            query = (
-                "MATCH (r:Recipe)-[:REQUIRES]->(i:Ingredient) WHERE i.name IN $ingredients "
-                "WITH r, count(i) AS matchingScore RETURN r, matchingScore ORDER BY matchingScore DESC"
-                + limitString
-            )
-            result = session.run(query, ingredients=ingredients)
+            result = session.run(
+                "MATCH (r:Recipe)-[:REQUIRES]->(i:Ingredient) WHERE i.name IN $ingredients WITH r, count(i) AS matchingScore RETURN r, matchingScore ORDER BY matchingScore DESC", ingredients=ingredients)
             data = [{"matchingScore": record['matchingScore'], "recipe": record['r']}
                     for record in result.data()]
 
-        response = jsonify(data)
+        response = jsonify({"recipes": data})
         response.headers.add("Access-Control-Allow-Origin", "*")
         return response
 
